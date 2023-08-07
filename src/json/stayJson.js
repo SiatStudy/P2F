a503c3031e953a5ad3574eea0383b62cca64b490
Randolph Carter <apink191550@gmail.com>
2023-08-07 14:51:51 +0900

    [ADD] add json file example data

---
+++ src/json/stayJson.js
@@ -0,0 +1,90 @@
+export const stayJson = [
+    {
+        title : "모텔",
+        src : `${process.env.PUBLIC_URL}/asset/img/main/MainPage_추천_img.jpg`,
+        text : {
+            star : 9.0,
+            name : "공공 모텔",
+            location : "[서울 / 중구]",
+            pay : "40.000원"
+        }
+    },
+    {
+        title : "모텔",
+        src : `${process.env.PUBLIC_URL}/asset/img/main/MainPage_추천_img.jpg`,
+        text : {
+            star : 8.25,
+            name : "일일 모텔",
+            location : "[서울 / 중구]",
+            pay : "50.000원"
+        }
+    },
+    {
+        title : "모텔",
+        src : `${process.env.PUBLIC_URL}/asset/img/main/MainPage_추천_img.jpg`,
+        text : {
+            star : 10.0,
+            name : "이이 모텔",
+            location : "[서울 / 중구]",
+            pay : "55.000원"
+        }
+    },
+    {
+        title : "호텔",
+        src : `${process.env.PUBLIC_URL}/asset/img/main/MainPage_추천_img.jpg`,
+        text : {
+            star : 10.0,
+            name : "이이 호텔",
+            location : "[서울 / 중구]",
+            pay : "75.000원"
+        }
+    },{
+        title : "호텔",
+        src : `${process.env.PUBLIC_URL}/asset/img/main/MainPage_추천_img.jpg`,
+        text : {
+            star : 10.0,
+            name : "이이 호텔",
+            location : "[서울 / 중구]",
+            pay : "97.000원"
+        }
+    },{
+        title : "호텔",
+        src : `${process.env.PUBLIC_URL}/asset/img/main/MainPage_추천_img.jpg`,
+        text : {
+            star : 10.0,
+            name : "이이 호텔",
+            location : "[서울 / 중구]",
+            pay : "129.000원"
+        }
+    },
+    {
+        title : "게스트 하우스",
+        src : `${process.env.PUBLIC_URL}/asset/img/main/MainPage_추천_img.jpg`,
+        text : {
+            star : 10.0,
+            name : "이이 게스트 하우스",
+            location : "[서울 / 중구]",
+            pay : "75.000원"
+        }
+    },
+    {
+        title : "게스트 하우스",
+        src : `${process.env.PUBLIC_URL}/asset/img/main/MainPage_추천_img.jpg`,
+        text : {
+            star : 10.0,
+            name : "이이 게스트 하우스",
+            location : "[서울 / 중구]",
+            pay : "155.000원"
+        }
+    },
+    {
+        title : "게스트 하우스",
+        src : `${process.env.PUBLIC_URL}/asset/img/main/MainPage_추천_img.jpg`,
+        text : {
+            star : 10.0,
+            name : "이이 게스트 하우스",
+            location : "[서울 / 중구]",
+            pay : "105.000원"
+        }
+    }
+]
