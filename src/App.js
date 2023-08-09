5ff7299cc1e85850fc279782d569f664befb897a
Randolph Carter <apink191550@gmail.com>
2023-08-10 03:15:11 +0900

    [SET] set router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
 import './App.css';
+import {Route, Routes} from "react-router-dom";
+
+import {MainPage} from "./pages/mainPage/MainPage";
+import {LocationPage} from "./pages/mainPage/LocationPage";
+import {SearchPage} from "./pages/mainPage/SearchPage";
+import {StayInfoPage} from "./pages/mainPage/StayInfoPage";
+import {StayPage} from "./pages/mainPage/StayPage";
+
+import {MyPage} from "./pages/userPage/MyPage";
+import {PaymentPage} from "./pages/userPage/PaymentPage";
+import {UserInfoPage} from "./pages/userPage/UserInfoPage";
+
+import {FindDataPage} from "./pages/userDataPage/FindDataPage";
+import {LoginPage} from "./pages/userDataPage/LoginPage";
+import {SignupPage} from "./pages/userDataPage/SignupPage";
 
 function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+    return (
+        <div className="App">
+            <Routes>
+                <Route path={"/"} element={<MainPage />} />
+                <Route path={"/login"} element={<LoginPage />} />
+                <Route path={"/users/signup"} element={<SignupPage />} />
+                <Route path={"/login/search/:userData"} element={<FindDataPage />} />
+                <Route path={"/stay/:stayData"} element={<StayPage />} />
+                <Route path={"/location/:locationData"} element={<LocationPage />} />
+                <Route path={"/search/:searchData"} element={<SearchPage />} />
+                <Route path={"/user/info"} element={<UserInfoPage />} />
+                <Route path={"/user/payment"} element={<PaymentPage />} />
+                <Route path={"/mypage"} element={<MyPage />} />
+                <Route path={"/product"} element={<StayInfoPage />} />
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 
 import App from './App';
 
 import './index.css';
+import store from "./core/storeConfig";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-      <BrowserRouter>
-          <App />
-      </BrowserRouter>
-  </React.StrictMode>
+const Index =() => (
+    <React.StrictMode>
+        <Provider store={store}>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </Provider>
+    </React.StrictMode>
 );
+
+const root = document.getElementById('root');
+const rootElement = createRoot(root);
+rootElement.render(<Index />);
